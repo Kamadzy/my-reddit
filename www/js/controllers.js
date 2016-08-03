3e7778914f7324cf3c37eb97bc1cf51b12dec954
Kirill <kam4513@gmail.com>
2016-08-03 18:51:38 +0300

    add request

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,23 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('RedditCtrl', function($http, $scope) {
+    $scope.stories = []
+    $http.get('https://www.reddit.com/r/Android/new/.json')
+      .success(function(response){
+        angular.forEach(response.data.children, function(child){
+          $scope.stories.push(child.data);
+        } );
+      })
+  })
+
+
+
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
+
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
